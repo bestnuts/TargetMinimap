87ea1640dd33e8a08c92345f2f68f5b6b637df65
bestnuts <pjh1204c7@gmail.com>
2026-04-07 12:39:38 +0900

    정사각형 AABB 검사

--- src/main/java/me/bestnuts/api/minimap/shape/SquareShape.java
+++ src/main/java/me/bestnuts/api/minimap/shape/SquareShape.java
@@ -6,6 +6,22 @@ public class SquareShape implements MinimapShape {
 
     @Override
     public boolean isRadiusIn(Location center, Location target, int radius) {
-        return false;
+        int centerX = center.getBlockX();
+        int centerY = center.getBlockZ();
+
+        int minX = centerX - radius;
+        int minY = centerY - radius;
+        int maxX = centerX + radius;
+        int maxY = centerY + radius;
+
+        int targetX = target.getBlockX();
+        int targetY = target.getBlockY();
+
+        return aabb(minX, minY, maxX, maxY, targetX, targetY);
+    }
+
+    private boolean aabb(int minX, int minY, int maxX, int maxY, int targetX, int targetY) {
+        return targetX >= minX && targetX <= maxX &&
+                targetY >= minY && targetY <= maxY;
     }
 }
